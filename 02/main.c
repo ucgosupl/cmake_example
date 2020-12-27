f9338cecb6e77953712c42770a2306de90dc8c20
Maciej Gajdzica <mgajdzica@gmail.com>
2020-12-27 23:07:54 +0100

    Add git branches generation

--- 02/main.c
+++ 02/main.c
@@ -10,9 +10,20 @@ int main(void)
     printf(STRINGIFY(VERSION_STRING));
     printf("\n%d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
+    printf("\n");
+    printf("Compiler name: ");
     printf(STRINGIFY(COMPILER_ID));
     printf("\n");
+    printf("Compiler path: ");
     printf(STRINGIFY(COMPILER));
+    printf("\n");
+
+    printf("\n");
+    printf("Git commit ID: ");
+    printf(STRINGIFY(GIT_COMMIT));
+    printf("\n");
+    printf("Git branch: ");
+    printf(STRINGIFY(GIT_BRANCH));
     printf("\n");
 
     return 0;
